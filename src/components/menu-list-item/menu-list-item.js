42bf9ab509f4167e5fa1d3264636fe8720f712db
Alex <alar0@yahoo.com>
2021-02-13 00:30:58 +0300

    Modify styles in itemPage.css, menu-list-item.scss and menu-list-item.js

--- src/components/menu-list-item/menu-list-item.js
+++ src/components/menu-list-item/menu-list-item.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "./menu-list-item.scss";
 import PropTypes from "prop-types";
-import salads from "../../img/salads.png";
-import pizza from "../../img/pizza.png";
-import meat from "../../img/meat.png";
 
 const MenuListItem = ({menuItem}) => {
     const {title, price, url, category} = menuItem;
-    const categoryImg = (category === "salads" ? salads
-                        : category === "meat" ? meat
-                        : category === "pizza" ? pizza : undefined);
     return (
         <li className="menu__item">
-            <div className="menu__title">{title}</div>
+            <div className="menu__title_sm">{title}</div>
             <img className="menu__img" src={url} alt={title}/>
-            <img align="right" src={categoryImg} alt={category}/>
             <div className="menu__category">Category: <span>{category}</span></div>
             <div className="menu__price">Price: <span>{price}$</span></div>
             <button className="menu__btn">Add to cart</button>
+            <span className = {`menu__catImg ${category}_big`}/>
         </li>
     );
 };
